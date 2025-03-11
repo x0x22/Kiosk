ce394ce68cb4246fc52ee4bfcf20bbfc7a6ec35d
isom <smallsol.fa@gmail.com>
2025-03-11 19:46:43 +0900

    주석수정

--- src/Lv2/MenuItem.java
+++ src/Lv2/MenuItem.java
@@ -3,9 +3,10 @@ package Lv2;
 import java.util.ArrayList;
 import java.util.List;
 
+//MenuItem 클래스 생성하기
 public class MenuItem {
 
-    // 속성
+    // 클래스는 이름, 가격, 설명 필드를 갖습니다.
     private String name;
     private double price;
     private String description;
@@ -31,8 +32,7 @@ public class MenuItem {
     //기능
 
     public void display() {
-        System.out.println(name + price);
-        System.out.println(description);
+        System.out.println(name + " | W " + price + " | " + description);
     }
 
 
